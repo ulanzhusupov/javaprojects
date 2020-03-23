e1a06a6f14e8c58c6327d0599f8a2b83c683ba15
Ulan <diz.dostuk@yandex.ru>
2020-03-23 21:45:17 +0300

    Initial commit

---
+++ springlibrary/src/main/java/com/ulanzhusupov/springlibrary/SpringlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ulanzhusupov.springlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlibraryApplication.class, args);
+	}
+
+}
---
+++ springlibrary/src/test/java/com/ulanzhusupov/springlibrary/SpringlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulanzhusupov.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
